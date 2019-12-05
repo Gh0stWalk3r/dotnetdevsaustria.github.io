6ac8486decd9bd2d602d8bdadf1d1f6bf5d0ba43
Andreas Willich <andreas@willich.io>
2019-12-05 19:38:57 +0100

    add cookie consent

---
+++ static/js/cookies.js
@@ -0,0 +1,17 @@
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#ffffff",
+      "text": "#000000"
+    }
+  },
+  "content": {
+    "message": "This website uses cookies to ensure you get the best experience on our website.",
+    "dismiss": "Got it!",
+    "link": "Learn more",
+    "href": "/impressum/#datenschutzerklrung"
+  }
+});
